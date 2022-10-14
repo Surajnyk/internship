f4a84a2ad241767b80bee704664c25017d40b7dc
Suraj Naik <113052112+Surajnyk@users.noreply.github.com>
2022-10-14 07:55:25 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,55 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.scss"
-import Navbar from "./components/Navbar/Navbar";
-import Contents from "./components/Navbar/Contents";
-import Home from "./components/Navbar/Home";
-import Background from "./components/Header/Background";
+import './App.css';
+import Web3 from "web3"
+import { useState, useEffect } from "react"
 
 function App() {
-return (
-    <BrowserRouter>
-    <Navbar />
-    <Background />
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/contents" element={<Contents />} />
-    </Routes>
-    </BrowserRouter>
+const [account, setAccount] = useState()
 
-);
+const [balance, setBalance] = useState()
+
+const [network, setNetwork] = useState()
+
+const web3 = new Web3(Web3.givenProvider)
+
+useEffect(()=>{
+  loadAccounts()
+}, [])
+
+useEffect(()=> {
+  loadBalance()
+},[account])
+
+async function loadAccounts(){
+  
+  const accounts = await web3.eth.requestAccounts();
+ 
+  setAccount(accounts[0])
+}
+
+async function loadBalance(){
+  
+  const network =await web3.eth.net.getNetworkType()
+ 
+  const balance =await web3.eth.getBalance(account)
+  
+  setNetwork(network)
+  setBalance(balance)
+}
+
+return (
+    <div className="App">
+      <header className="App-header">
+        Web3.React wallet App 
+    <p>
+        Connected Account : ({account})
+    </p>
+    <p>
+      Current Balance ({network}) :
+      ETH {(balance/1e18).toFixed(2)}
+    </p>
+       </header>
+    </div>
+  );
 }
 
 export default App;
